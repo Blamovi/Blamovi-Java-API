fa282426289cc3c7c3faf9b30ace6f71d8b9408a
Flamarion <flamarionsouza23@gmail.com.br>
2023-12-08 19:42:23 -0300

    finalizado

---
+++ src/main/java/com/senai/apiblamovi/dtos/ComentarioDto.java
@@ -0,0 +1,10 @@
+package com.senai.apiblamovi.dtos;
+
+import java.util.UUID;
+
+public record ComentarioDto(
+        UUID idComentario,
+        UUID id_usuario,
+        UUID idPost,
+        String texto
+) {}
